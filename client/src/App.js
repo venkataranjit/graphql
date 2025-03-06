d5769d6584f22364f585f562c7385f8a9082bae6
venkataranjit <venkataranjit@live.com>
2025-03-06 23:25:53 +0530

    new commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
+
+function App() {
+  const { data, loading } = useQuery(query);
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          {data.getTodos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo?.user?.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
